9c2521ebe1ac38a9b682a403492ec083c6f9f692
Justin <jtener@udel.edu>
2023-02-25 19:05:17 -0500

    cleaned docs, switched to compat, fixed user pics

--- public/script.js
+++ public/script.js
@@ -1,11 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import * as rtdb from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBJDoJS1DR66szG3_X-OAUNNnXr19n3__Y",
   authDomain: "jtener-websec-2022.firebaseapp.com",
@@ -16,26 +8,36 @@ const firebaseConfig = {
   appId: "1:792031619120:web:4c79b9b85b44ca40f6a5f5",
   measurementId: "G-YPJKE61V29"
 };
+firebase.initializeApp(firebaseConfig);
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-let db = rtdb.getDatabase(app);
+const twootsRef = firebase.database().ref("/twoots");
+const usersRef = firebase.database().ref("/users");
 
-// Function to render a twoot
-/* Sources:
-https://drapak.ca/cpg/htmlConvertUnicode.shtml#:~:text=You%20do%20this%20by%20adding,will%20display%20J%20in%20HTML.
-https://getbootstrap.com/docs/5.3/components/card/
-https://symbl.cc/en/2764/
-https://symbl.cc/en/1F502/
-https://stackoverflow.com/questions/30192263/bootstrap-javascript-not-working
-https://www.w3schools.com/tags/tag_script.asp
-https://makeschool.org/mediabook/oa/tracks/web-dev-with-node/your-first-website--landing-page/grouping-elements/
+
+/*
+Renders a twoot with some complex CSS styling.
+
+Sources:
+  Bootstrap card component for twoot design  https://getbootstrap.com/docs/5.3/components/card/
+  Creating Unicode characters in HTML        https://drapak.ca/cpg/htmlConvertUnicode.shtml
+  Black Heart Symbol Emoji                   https://symbl.cc/en/2764/
+  Circular Arrows Emoji                      https://symbl.cc/en/1F502/
+  Bootstrap JS troubleshooting               https://stackoverflow.com/questions/30192263/bootstrap-javascript-not-working
+  HTML script Tag                            https://www.w3schools.com/tags/tag_script.asp
+  Grouping HTML elements                     https://makeschool.org/mediabook/oa/tracks/web-dev-with-node/your-first-website--landing-page/grouping-elements/
+  HTML URL Encoding Reference                https://www.w3schools.com/tags/ref_urlencode.ASP
+
+Parameters:
+  {object} twoot - A JSON object representing a twoot
+  {string} userPic - A web link for a user profile picture
+
 */
-let renderTwoot = (tObj, userPic) => {
+const renderTwoot = (twoot, userPic) => {
 
   const heart = `&#${10084}`;
   const arrow = `&#${128258}`;
 
+  /* Bootstrap card component */
   $("#everytwoot").append(`
     <div class="card mb-3" style="max-width: 540px;">
       <div class="row g-0" id="pic">
@@ -44,9 +46,9 @@ let renderTwoot = (tObj, userPic) => {
         </div>
         <div class="col-md-8">
           <div class="card-body">
-            <h5 class="card-title">${tObj.handle}</h5>
-            <p class="card-text">${tObj.text}</p>
-            <p class="card-text">${heart} ${tObj.likes}  ${arrow} ${tObj.retwoots} -- <small class="text-muted">${tObj.timestamp}</small></p>
+            <h5 class="card-title">${twoot.handle}</h5>
+            <p class="card-text">${twoot.text}</p>
+            <p class="card-text">${heart} ${twoot.likes}  ${arrow} ${twoot.retwoots} -- <small class="text-muted">${twoot.timestamp}</small></p>
           </div>
         </div>
       </div>
@@ -54,9 +56,18 @@ let renderTwoot = (tObj, userPic) => {
   `);
 }
 
-// Add a new twoot to the database
-// Source: https://firebase.google.com/docs/database/web/read-and-write#updating_or_deleting_data
-let createTwoot = (handle, text) => {
+/*
+Adds a new twoot to the database.
+
+Sources:
+  Pushing to database  https://firebase.google.com/docs/database/web/read-and-write#updating_or_deleting_data
+
+Parameters:
+  {string} handle - Uniquely identifies a Twutter user
+  {string} text - The content of the new twoot
+
+*/
+const createTwoot = (handle, text) => {
 
   const twootContent = {
     handle: handle,
@@ -66,23 +77,25 @@ let createTwoot = (handle, text) => {
     timestamp: new Date().toLocaleString()
   };
 
-  const twootsRef = rtdb.ref(db, "/twoots");
-  return rtdb.push(twootsRef, twootContent);
+  twootsRef.push(twootContent);
 };
 
-/* Sources:
-https://www.w3schools.com/html/tryit.asp?filename=tryhtml_id_js
-https://www.w3schools.com/jsref/met_document_getelementbyid.asp
-https://www.w3schools.com/jsref/prop_html_innerhtml.asp
-https://linuxhint.com/get-element-value-using-javascript/#:~:text=To%20get%20the%20value%20of%20the%20DOM's%20element%2C%20use%20the,the%20%E2%80%9CquerySelector()%E2%80%9D%20method.
-https://blog.hubspot.com/website/call-javascript-function-html#:~:text=Use%20Script%20Tags,interacts%20with%20a%20page%20element.
-https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
-https://careerkarma.com/blog/javascript-replace/#:~:text=To%20replace%20text%20in%20a,replace%20text%20in%20a%20string.
-https://stackoverflow.com/questions/10826756/javascript-innerhtml-not-updating-element
+/*
+Click listener for the "Publish" button, enabling the creation of new twoots
+
+Sources:
+  getElementById().innerHTML code snippet  https://www.w3schools.com/html/tryit.asp?filename=tryhtml_id_js
+  HTML DOM Document getElementById()       https://www.w3schools.com/jsref/met_document_getelementbyid.asp
+  HTML DOM Element innerHTML               https://www.w3schools.com/jsref/prop_html_innerhtml.asp
+  .value and other DOM elements            https://linuxhint.com/get-element-value-using-javascript
+  HTML script tags and external JS files   https://blog.hubspot.com/website/call-javascript-function-html
+  JS strings and string manipulation       https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
+  replace()                                https://careerkarma.com/blog/javascript-replace
+  innerHTML troubleshooting                https://stackoverflow.com/questions/10826756/javascript-innerhtml-not-updating-element
+
 */
 $("#publish-twoot").on("click", evt => {
 
-  console.log("CLICK!");
   const input = document.getElementById("input-twoot");
   const inputVal = input.value;
   if(inputVal != "") {
@@ -91,24 +104,38 @@ $("#publish-twoot").on("click", evt => {
   }
 })
 
-// Create twoots
-// createTwoot("JtenerWebSec", "If you can't do it good, do it hard.");
-// createTwoot("bobby231", "I love grilling");
-// createTwoot("poopyHead63", "No profile picture for me!");
+/*
+Loads a user profile picture and returns it using a callback function
 
-// Render twoots
-let twootsRef = rtdb.ref(db, "/twoots");
-rtdb.onChildAdded(twootsRef, ss => {
-  const pic = "https://icon-library.com/images/default-profile-icon/default-profile-icon-24.jpg";
-  renderTwoot(ss.val(), pic);
-  // rtdb.get(db, `/users/${ss.val().handle}`)
-  // .then(ss2 => {
-  //   if(ss2.exists) {
-  //     pic = ss2;
-  //   }
-  //   renderTwoot(ss, pic);
-  // })
-  // .catch(error => {
-  //   console.error(`ERROR! ${error}`);
-  // });
+Sources:
+  Callbacks                     https://youtu.be/kz_vwAF4NHI
+  Read and write in compat mode https://firebase.google.com/docs/database/web/read-and-write#web-version-8_3
+
+Parameters:
+  {string} handle - The user's handle, essentially their unique user ID
+  {function} cb - A callback function
+ */
+const loadUserPic = (handle, cb) => {
+  usersRef.child(handle).child("picture").get().then(ss => {
+    if(ss.exists()) {
+      cb(ss.val());
+    } else {
+      cb("https://icon-library.com/images/default-profile-icon/default-profile-icon-24.jpg");
+    }
+  });
+}
+
+/*
+An event listener that loads a user's profile picture and uses a callback function to render a twoot
+
+Sources:
+  On child added                https://firebase.google.com/docs/database/admin/retrieve-data#node.js_1
+  Callbacks                     https://youtu.be/kz_vwAF4NHI
+  Read and write in compat mode https://firebase.google.com/docs/database/web/read-and-write#web-version-8_3
+
+*/
+twootsRef.on("child_added", ss => {
+  loadUserPic(ss.val().handle, pic => {
+    renderTwoot(ss.val(), pic);
+  });
 });
